5aab351a7661cb231112b3b78aa5a375ece9cf0b
Toby Penk <tobypenk@gmail.com>
2021-06-13 16:00:30 -0400

    fix self-reference issue in ScraperResult class

--- scraper/scrapers/ScraperResult.py
+++ scraper/scrapers/ScraperResult.py
@@ -8,34 +8,44 @@ class ScraperResult:
 		self.timing = timing
 
 	def get_payload(self):
-		self.payload = {
-			"title": self.title,
-			"steps": self.steps,
-			"ingredients": self.ingredients,
-			"yield": self.recipe_yield,
-			"timing": self.timing
-		}
+		
+		if not hasattr(self,"payload"):
+			self.payload = {
+				"title": self.title,
+				"steps": self.steps,
+				"ingredients": self.ingredients,
+				"yield": self.recipe_yield,
+				"timing": self.timing
+			}
+			
 		return self.payload
-
-	def map_payload_to_known_ingredients(self,ing_cache,corpus,classifier):
+		
+	def map_payload_ingredients(self,ing_cache,corpus,classifier,global_hyperparameters):
 		
 		from tensorflow.keras.preprocessing.sequence import pad_sequences
-		if hasattr(self,"payload") == False:
-			self.get_payload()
-
-		tokens = corpus.tokenizer.texts_to_sequences(self.payload.ingredients)
-		# need to bring in max_length,padding_type,trunc_type
-		tokens = pad_sequences(
-			tokens, 
-			maxlen=max_length, 
-			padding=padding_type, 
-			truncating=trunc_type
-		)
-		pred = classifier.model.predict(tokens)
+		import numpy as np
 		
-		for i in range(len(self.payload.ingredients)):
-			pred_id = np.argmax(pred[i])
-			matched_ing = list(filter(lambda x: x[1] == pred_id,ing_cache))[0]
-			self.payload.ingredients[i] = [self.payload.ingredients[i],matched_ing[0],pred_id]
-			    
-			    
+		if not hasattr(self,"payload"):
+			self.get_payload()
+			
+		if not hasattr(self.payload,"mapped_ingredients"):
+		
+			self.payload["mapped_ingredients"] = []
+			
+			tokens = corpus.tokenizer.texts_to_sequences(self.payload["ingredients"])
+			tokens = pad_sequences(
+				tokens, 
+				maxlen=global_hyperparameters["max_length"], 
+				padding=global_hyperparameters["padding_type"], 
+				truncating=global_hyperparameters["trunc_type"]
+			)
+			
+			pred = classifier.model.predict(tokens)
+			
+			for i in range(len(self.payload["ingredients"])):
+				pred_id = np.argmax(pred[i])
+				matched_ing = list(filter(lambda x: x[1] == pred_id,ing_cache))[0]
+				self.payload["mapped_ingredients"].append([self.payload["ingredients"][i],matched_ing[0],pred_id])
+				
+		return self.payload
+			
