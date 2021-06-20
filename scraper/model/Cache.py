12cd0e53e1c6060917a7c4a7be529a47d94ebddb
Toby Penk <tobypenk@gmail.com>
2021-06-20 13:41:10 -0400

    reformat SQL queries in cache class to be more readable

--- scraper/model/Cache.py
+++ scraper/model/Cache.py
@@ -10,3 +10,16 @@ class Cache:
             self.ingredients = Query("select singular_name, id from ingredient;").get_data()
 
         return self.ingredients
+        
+    def get_measures(self):
+        from database.Query import Query
+        
+        if not hasattr(self,'measures'):
+            self.measures = Query("select "+
+            	"short_name "+
+            	", long_name "+
+            	", plural_name "+
+            	", long_plural "+
+            	"from measure where is_common = 1;").get_data()
+
+        return self.measures
