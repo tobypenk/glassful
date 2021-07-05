37ed00ad234ca3a31e48881a56419e81b3da12ca
Toby Penk <tobypenk@gmail.com>
2021-07-04 20:43:38 -0400

    add Read class and update functionality

--- scraper/database/Read.py
+++ scraper/database/Read.py
@@ -1,35 +1,33 @@
-class Query:
+class Read:
 	
 	def __init__(self, query_string):
 		self.query_string = query_string
 		
-	def get_data(self):
+	def get_data(self,force_update=False):
 		
-		from database.Connection import Connection
-		from database.Credentials import Credentials
-		
-		creds = Credentials()
-		
-		con = Connection(
-			host = creds.host,
-			user = creds.user,
-			pwd = creds.pwd,
-			dbname = creds.dbname
-		)
-		
-		self.result = []
-		
-		with con.connection() as connection:
-			with connection.cursor() as cursor:
-				cursor.execute(self.query_string)
-				result = cursor.fetchall()
-				for row in result:
-					self.result.append(row)
+		if force_update or not hasattr(self,"result"):
+			
+			from database.Connection import Connection
+			from database.Credentials import Credentials
+			
+			creds = Credentials()
+			
+			con = Connection(
+				host = creds.host,
+				user = creds.user,
+				pwd = creds.pwd,
+				dbname = creds.dbname
+			)
+			
+			self.result = []
+			
+			with con.connection() as connection:
+				with connection.cursor() as cursor:
+					cursor.execute(self.query_string)
+					result = cursor.fetchall()
+					for row in result:
+						self.result.append(row)
 					
 		return self.result
 		
-	def insert(self):
-		pass
-		
-	def insert_and_return_insert_id(self):
-		
+	
